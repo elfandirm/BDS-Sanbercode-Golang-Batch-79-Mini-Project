2d71fb5e7b14958332c427450474ad8d509ade58
elfandirm <elfandi.rm@gmail.com>
2026-08-15 12:08:57 +0700

    fix(db): fix db config

--- config/database.go
+++ config/database.go
@@ -2,21 +2,26 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func ConnectDatabase() (*sql.DB, error) {
-	dsn := "postgres://postgres:1234567890@localhost:5432/bioskop_db"
+	dsn := os.Getenv("DATABASE_URL")
+
+	if dsn == "" {
+		dsn = "postgres://postgres:1234567890@localhost:5432/bioskop_db"
+	}
 
 	db, err := sql.Open("pgx", dsn)
 	if err != nil {
 		return nil, err
 	}
 
-	err = db.Ping()
-	if err != nil {
-		return nil, err
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("gagal ping database: %w", err)
 	}
 
 	return db, nil
